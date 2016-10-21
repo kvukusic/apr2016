2b384b0c6aa3abadf421bff9ee48ba36a6522825
Kristian Vukušić <kristian.vukusic@gmail.com>
2016-10-22 00:41:23 +0200

    [DZ1] Add some additional info

--- DZ1/Demo/Assignments/Assignment4.cs
+++ DZ1/Demo/Assignments/Assignment4.cs
@@ -11,6 +11,10 @@ namespace DZ1.Demo.Assignments
 
             LinearEquationSolver.SolveLU(a, b);
             LinearEquationSolver.SolveLUP(a, b);
+
+            // The difference between LU and LUP is that if you divide by smaller
+            // numbers you get a smaller error, bit if you divide by larger numbers
+            // you get a larger error (this is referred to as stability of the algorithm)
         }
     }
 }
