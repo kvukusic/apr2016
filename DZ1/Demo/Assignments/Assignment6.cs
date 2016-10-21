f7a64be0acf2b376926d517b7e6fa8ed86bc0f21
Kristian Vukušić <kristian.vukusic@gmail.com>
2016-10-22 00:41:39 +0200

    [DZ1] Add example with scaling

--- DZ1/Demo/Assignments/Assignment6.cs
+++ DZ1/Demo/Assignments/Assignment6.cs
@@ -9,8 +9,12 @@ namespace DZ1.Demo.Assignments
             Matrix a = new Matrix("Input/M6_A.txt");
             Matrix b = new Matrix("Input/M6_B.txt");
 
-            LinearEquationSolver.SolveLU(a, b);
-            LinearEquationSolver.SolveLUP(a, b);
+            LinearEquationSolver.SolveLU(a*10e3, b*10e3);
+            LinearEquationSolver.SolveLUP(a*10e3, b*10e3);
+
+            double scale = 10e3;
+            LinearEquationSolver.SolveLU(a*scale, b*scale);
+            LinearEquationSolver.SolveLUP(a*scale, b*scale);
         }
     }
 }
