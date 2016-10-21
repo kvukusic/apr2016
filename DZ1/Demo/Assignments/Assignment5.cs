a75b976210804603415a1656652050e3b30788be
Kristian Vukušić <kristian.vukusic@gmail.com>
2016-10-22 01:13:15 +0200

    [DZ1] Add explanation

--- DZ1/Demo/Assignments/Assignment5.cs
+++ DZ1/Demo/Assignments/Assignment5.cs
@@ -11,6 +11,13 @@ namespace DZ1.Demo.Assignments
 
             LinearEquationSolver.SolveLU(a, b);
             LinearEquationSolver.SolveLUP(a, b);
+
+            // Because there are no fractions there is a slight error in real
+            // result vs the computer result
+            // For example 9 - 3*2/3 will result in 7 on paper, but 7.0002 in
+            // a double variable if we treat 2/3 as 0.6666
+            // The error is smaller the more precision we use, in C# double it
+            // is the precision of 15 decimals
         }
     }
 }
