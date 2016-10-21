e45832e170f7c0acee6547c5150f509d00273087
Kristian Vukušić <kristian.vukusic@gmail.com>
2016-10-22 01:16:43 +0200

    [DZ1] Add explanation

--- DZ1/Demo/Assignments/Assignment6.cs
+++ DZ1/Demo/Assignments/Assignment6.cs
@@ -15,6 +15,11 @@ namespace DZ1.Demo.Assignments
             double scale = 10e3;
             LinearEquationSolver.SolveLU(a*scale, b*scale);
             LinearEquationSolver.SolveLUP(a*scale, b*scale);
+
+            // If we use EPSILON 10e-6 the algorithm will fail because we treat
+            // 0.0000000003 as zero and return as divide by zero error.
+            // The solution is to multiply the whole system with a scale factor
+            // so that there is a value larger than the matrix precision.
         }
     }
 }
