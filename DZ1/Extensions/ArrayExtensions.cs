8b5c6e4df4c27e4df11182b4468a8e45a52599de
Kristian Vukušić <kristian.vukusic@gmail.com>
2016-10-28 17:00:14 +0200

    [DZ1] Add vector representation and some common array extensions

---
+++ DZ1/Extensions/ArrayExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace APR.DZ1.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static T[] Copy<T>(this T[] src)
+        {
+            var dest = new T[src.Length];
+            Array.Copy(src, dest, dest.Length);
+            return dest;
+        }
+
+        public static double[] Copy(this double[] src)
+        {
+            var dest = new double[src.Length];
+            Array.Copy(src, dest, dest.Length);
+            return dest;
+        }
+
+        public static string Format(this double[] src, int precision = 3)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("[");
+
+            for (int i = 0; i < src.Length; i++)
+            {
+                sb.Append(src[i].ToString("F" + precision));
+                if (i != src.Length - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            sb.Append("]");
+
+            return sb.ToString();
+        }
+
+        public static Vector ToVector(this double[] src)
+        {
+            return new Vector(src.Copy());
+        }
+
+        public static double Norm(this double[] src)
+        {
+            var srcCopy = src.Copy();
+            return Math.Sqrt(srcCopy.Sum(d => d*d));
+        }
+    }
+}
--- DZ1/Math/Vector.cs
+++ DZ1/Math/Vector.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Text;
+using APR.DZ1.Extensions;
 
 namespace APR.DZ1
 {
     public class Vector
     {
-        
+        public static readonly int PRECISION = 6;
+        public static readonly double EPSILON = Math.Pow(10, -PRECISION);
+
+        private double[] _values;
+        private bool _isTransposed;
+
+        public Vector (int dimension)
+        {
+            if(dimension <= 0)
+            {
+                throw new ArgumentException("Negative or zero dimension.");
+            }
+        }
+
+        public Vector(params double[] values)
+        {
+            if(values == null)
+            {
+                throw new ArgumentNullException("values is null");
+            }
+
+            _values = values.Copy();
+        }
+
+        public int Dimension { get { return _values.Length; } }
+
+        public double Norm()
+        {
+            return _values.Norm();
+        }
+
+        public void Transpose()
+        {
+            _isTransposed = !_isTransposed;
+        }
+
+        public double this[int index]
+        {
+            get { return _values[index]; }
+            set { _values[index] = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+            for (int i = 0; i < _values.Length; i++)
+            {
+                sb.Append(String.Format("{0:F" + PRECISION + "}", _values[i]));
+                if (i < _values.Length - 1)
+                {
+                    sb.Append(',');
+                }
+            }
+            sb.Append(']');
+            return sb.ToString();
+        }
     }
 }
