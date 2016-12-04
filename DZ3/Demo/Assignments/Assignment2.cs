14edaad0e8ced9a8ab7d371ffb461ec29ecc7a25
Kristian Vukušić <kristian.vukusic@gmail.com>
2016-12-04 04:39:26 +0100

    [DZ3] Finish 2nd assignment

--- DZ3/Demo/Assignments/Assignment2.cs
+++ DZ3/Demo/Assignments/Assignment2.cs
@@ -1,4 +1,5 @@
 using System;
+using APR.DZ2.Functions;
 
 namespace APR.DZ3.Demo.Assignments
 {
@@ -6,7 +7,25 @@ namespace APR.DZ3.Demo.Assignments
     {
         public void Run()
         {
-            throw new NotImplementedException();
+            var function1 = new F1();
+            var function2 = new F2();
+
+            var method1 = new SteepestDescentMethod();
+            method1.IsOutputPerIterationEnabled = false;
+
+            method1.OptimizeStepSize = true;
+            method1.Minimize(function1, new double[] { -1.9, 2.0 });
+
+            method1.OptimizeStepSize = true;
+            method1.Minimize(function2, new double[] { 0.1, 0.3 });
+
+            var method2 = new NewtonRaphsonMethod();
+
+            method2.OptimizeStepSize = true;
+            method2.Minimize(function1, new double[] { -1.9, 2.0 });
+
+            method2.OptimizeStepSize = true;
+            method2.Minimize(function2, new double[] { 0.1, 0.3 });
         }
     }
 }
