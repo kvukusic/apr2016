e12019eb81b0b8eaf1dc42b0a35ef8258f978aa3
Kristian Vukušić <kristian.vukusic@gmail.com>
2016-12-14 16:43:12 +0100

    [DZ4] Finish 4th assignment

--- DZ4/Demo/Assignments/Assignment4.cs
+++ DZ4/Demo/Assignments/Assignment4.cs
@@ -8,7 +8,17 @@ namespace APR.DZ4.Demo.Assignments
     {
         public void Run()
         {
-            throw new NotImplementedException();
+            var ga = new GeneticAlgorithm(new GAFunction(new F6(), 2, new GAFunction.Constraint(-150, 50)));
+            ga.Encoding = ChromosomeEncoding.FloatingPoint;
+            ga.CrossoverOperator = CrossoverOperator.Arithmetic;
+            ga.StopEval = null;
+            ga.StopValue = 10e-6;
+            ga.StopNew = 1000;
+            ga.Precision = 6;
+            ga.PopulationSize = 30;
+            ga.MutationRate = 2;
+            ga.Initialize();
+            ga.Run();
         }
     }
 }
